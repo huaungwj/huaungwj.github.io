d39faf006f5e3df27144953e3b053da4bece435d
weiji <1835773652@qq.com>
2020-04-07 20:33:47 +0800

    Site updated: 2020-04-07 20:33:46

--- dist/music.js
+++ dist/music.js
@@ -3,54 +3,30 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true,
     audio: [
-	  {
-        name: '勇气',
-        artist: '棉子',
-        url: 'http://music.163.com/song/media/outer/url?id=1411358329.mp3',
-        cover: '/images/5.jpg',
-      },
-      {
-        name: "星星",
-        artist: '张杰',
-        url: 'http://music.163.com/song/media/outer/url?id=1399004693.mp3',
-        cover: '/images/xingxing.jpg',
-      },
-      {
-        name: '这就是爱吗',
-        artist: '十豆',
-        url: 'http://music.163.com/song/media/outer/url?id=1412242872.mp3',
-        cover: '/images/zjsam.jpg',
-      },
-      {
-        name: 'Alone',
-        artist: 'Alan Walker / Noonie Bao',
-        url: 'http://music.163.com/song/media/outer/url?id=444269135.mp3',
-        cover: '/images/alone.jpg',
-      },
-	  {
-        name: 'Umbrella (Matte Remix)',
-        artist: 'Matte / Ember Island',
-        url: 'http://music.163.com/song/media/outer/url?id=164209623.mp3',
-        cover: '/images/1.jpg',
-      },
-	  {
-        name: '大眠 (完整版)原唱：王心凌',
-        artist: '小乐哥',
-        url: 'http://music.163.com/song/media/outer/url?id=3778678.mp3',
-        cover: '/images/2.jpg',
-      },
-	  {
-        name: '世间美好与你环环相扣',
-        artist: '柏松',
-        url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',
-        cover: '/images/3.jpg',
-      },
-	  {
-        name: '飞',
-        artist: '王恩信Est / 二胖u',
-        url: 'http://music.163.com/song/media/outer/url?id=1386259535.mp3',
-        cover: '/images/4.jpg',
-      }
+        $.ajax({
+            url: "https://api.i-meto.com/meting/api?server=netease&type=playlist&id=141998290",
+            success: function(e) {
+                var aplayerList = new APlayer({
+                element: document.getElementById('player'),
+                narrow: false,
+                autoplay: true,
+                showlrc: false,
+                mutex: true,
+                theme: '#FFF0',
+                mode: 'random',
+                preload: 'metadata',
+                listmaxheight: '200px',
+                music:JSON.parse(e)
+                });
+                window.aplayers || (window.aplayers = []),
+                window.aplayers.push(aplayerList)
+            }
+        })
     ]
 });
+
+var ls  = document.getElementById('player');
+ls.style.boxShadow = 'none';
+ls.style.marginTop = '10px';
+$('.ls-list-light').css('background','#d8e2eb69');
 

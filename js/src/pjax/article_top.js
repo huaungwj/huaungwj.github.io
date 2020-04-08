5bceb0d9395ef78fb7e9121031ec7f85f91702a1
weiji <1835773652@qq.com>
2020-04-08 19:16:59 +0800

    Site updated: 2020-04-08 19:16:59

---
+++ js/src/pjax/article_top.js
@@ -0,0 +1,3 @@
+// build time:Tue Feb 18 2020 09:23:34 GMT+0800 (China Standard Time)
+function article_top(){function t(){var t=window.location.pathname;var i=$(".header").innerHeight();if(t.indexOf("html")>0){$("html,article").animate({scrollTop:i},300)}}setTimeout(t,800)}article_top();
+//rebuild by neat 
---
+++ js/src/pjax/share.js
@@ -0,0 +1,3 @@
+// build time:Tue Feb 18 2020 09:23:34 GMT+0800 (China Standard Time)
+define(["jquery.share.min"],function(){var e={sites:["weibo","qq","wechat","qzone","douban","facebook","google","twitter"],disabled:["linkedin","diandian"],wechatQrcodeTitle:"微信扫一扫",wechatQrcodeHelper:"<p>微信扫一扫，右上角分享</p>",source:"Leesin Dong"};$(".post-spread").share(e);function o(){$(".post-spread").share(e)}});
+//rebuild by neat 
